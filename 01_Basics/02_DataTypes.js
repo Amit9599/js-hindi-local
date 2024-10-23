711b402018813a9d4d19c0a0d5d54349fb9c5fa8
amitrssb <amit.singh@broadwaylive.in>
2024-10-23 17:38:31 +0530

    update data types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as new version
+
+// alert (3 + 3) // alert not working in node js, its working in browser
+
+console.log(2
+    +3); // code readability should be high
+    console.log("Amit"); // this code readability is high
+
+let a = 1234;
+let b = "string";
+let isLogIn = true;
+
+// number => 2 to the power of 53
+// bigint use after number 
+// string can type in "" and ''
+// boolean value is true and false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "hello");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof a);
