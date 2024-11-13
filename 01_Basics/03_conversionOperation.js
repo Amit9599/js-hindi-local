8d093af1f6d113855eb1114ffba8a29fa0559f1f
amitrssb <amit.singh@broadwaylive.in>
2024-11-13 17:41:07 +0530

    update conversion datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,3 +21,36 @@ let someNumber = 12;
 let changeInstring = String(someNumber);
 console.log(changeInstring);
 console.log(typeof changeInstring);
+
+let sValue = 3;
+let negativeValue = -sValue;
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(3-5);
+console.log(2*2);
+console.log(2**4);
+console.log(2/3);
+console.log(1345%3);
+
+let str1 = "blue";
+let str2 = " berry";
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log("1" + 2 + "3"); // 123
+console.log(1 + 2 + "4"); // 34
+console.log(1 + "2" + "5"); // 125
+console.log("1" + 2 + 4); // 124
+console.log(1 + "2" + 6 + 3); // 1263
+console.log(3 + 4 * 5 % 3); //Bodmas 
+
+console.log(+true); // 1
+console.log(+false) // 0
+
+console.log(+""); // 0
+
+let gameCounter = 3;
+gameCounter--;
+console.log(gameCounter);
